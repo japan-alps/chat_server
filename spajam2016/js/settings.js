6d8d444fc21cf0e323263c928d37d0d1c3ab4078
internsaka <b1013027@gmail.com>
2016-05-29 20:23:33 +0000

    fix

--- spajam2016/js/settings.js
+++ spajam2016/js/settings.js
@@ -1,4 +1,4 @@
-exports.db_host = 'localhost';
-exports.db = 'spajam2016';
-exports.db_user = 'b1013027';
-exports.db_pass = 'spajam2016';
+exports.db_host = '';
+exports.db = '';
+exports.db_user = '';
+exports.db_pass = '';
